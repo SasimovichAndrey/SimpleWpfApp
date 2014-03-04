9557149eb5bcba85025474d8dd496f766891f5a3
Sinitsin <ballack.tyta@gmail.com>
2014-03-04 08:05:19 +0300

    Debugging two-way binding

--- FiringField.xaml.cs
+++ FiringField.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SimpleWpfApp.Model;
+using SimpleWpfApp.Converters;
 
 namespace SimpleWpfApp
 {
@@ -22,6 +24,21 @@ namespace SimpleWpfApp
         public FiringField()
         {
             InitializeComponent();
+            try
+            {
+                Employee empl = new Employee() { FirstName = "Oleg", SecondName ="Atman"};
+                Binding binding = new Binding();
+                binding.Converter = new StringEmployeeConverter();
+                binding.Source = empl;
+                binding.Path = null;
+                binding.Mode = BindingMode.TwoWay;
+                BindingTextBox.SetBinding(TextBox.TextProperty, binding);
+                BindingLabel.SetBinding(Label.ContentProperty, binding);
+            }
+            catch (Exception e)
+            {
+
+            }
         }
 
     }
--- Model/Employee.cs
+++ Model/Employee.cs
@@ -2,12 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace SimpleWpfApp.Model
 {
-    class Employee
+    public class Employee : INotifyPropertyChanged
     {
-        public String FirstName { get; set; }
-        public String SecondName { get; set; }
+        private String firstName;
+        private String secondName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public String SecondName
+        {
+            get
+            {
+                return this.secondName;
+            }
+            set
+            {
+                this.secondName = value;
+                ChangedNotify("SecondName");
+            }
+        }
+
+        public String FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName = value;
+                ChangedNotify("FirstName");
+            }
+        }
+
+        protected void ChangedNotify(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
