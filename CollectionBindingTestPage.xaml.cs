d0ce3edb363fa1b62253d0c9b61243d23744ed02
unknown <Andrei_Sasimovich@EPBYGROW0198.grodno.epam.com>
2014-03-05 11:32:04 +0300

    Added first collection binding. Added first DataTemplate, sorting and filtering to the collection view

--- CollectionBindingTestPage.xaml.cs
+++ CollectionBindingTestPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SimpleWpfApp.Model;
+using System.ComponentModel;
 
 namespace SimpleWpfApp
 {
@@ -24,11 +25,42 @@ namespace SimpleWpfApp
         {
             InitializeComponent();
             List<Employee> list = new List<Employee>();
-            list.Add(new Employee() { FirstName = "Wania", SecondName="Kovalev" });
-            list.Add(new Employee() { FirstName = "Evgeny", SecondName = "Solovyov" });
+            list.Add(new Employee() { FirstName = "AEvgeny", SecondName = "Solovyov" });
+            list.Add(new Employee() { FirstName = "ZWasia", SecondName = "Ozboarne" });
+            list.Add(new Employee() { FirstName = "CMichael", SecondName = "Smith" });
+            list.Add(new Employee() { FirstName = "BWasia", SecondName = "Kovalev" });
             EmployeeList emplList = new EmployeeList(){List = list};
 
-            PageGrid.DataContext = emplList;
+            this.DataContext = emplList;
+        }
+
+        private void AddSorting(object sender, RoutedEventArgs e)
+        {
+            ((CollectionViewSource)this.Resources["VSource"]).SortDescriptions.Add(new SortDescription("FirstName", ListSortDirection.Ascending));
+        }
+
+        private void RemoveSorting(object sender, RoutedEventArgs e)
+        {
+            ((CollectionViewSource)this.Resources["VSource"]).SortDescriptions.Clear();
+        }
+
+        private void WasiaFilter(object sender, FilterEventArgs e)
+        {
+            Employee empl = (Employee)e.Item;
+            if (empl.FirstName.Contains("Wasia"))
+                e.Accepted = true;
+            else
+                e.Accepted = false;
+        }
+
+        private void AddFiltering(object sender, RoutedEventArgs e)
+        {
+            ((CollectionViewSource)this.Resources["VSource2"]).Filter += WasiaFilter;
+        }
+
+        private void RemoveFiltering(object sender, RoutedEventArgs e)
+        {
+            ((CollectionViewSource)this.Resources["VSource2"]).Filter -= WasiaFilter;
         }
     }
 }
